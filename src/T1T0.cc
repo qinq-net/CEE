9afde568cb369c4b418bf63bdb4f66afcea38648
qz-ubuntu <enpg_qz@protonmail.com>
2018-04-28 20:02:11 +0800

    TPC

--- src/T1T0.cc
+++ src/T1T0.cc
@@ -26,6 +26,7 @@
 #include <string>
 #include <vector>
 #include "StringSplit.hh"
+#include "ReadLine.hh"
 
 using namespace std;
 /*
@@ -48,19 +49,19 @@ T1T0::T1T0()
   outFile<<"0 0 0 0 0 0"<<endl;
   outFile<<"#2 rmin;rmax;T0的dz（全长）"<<endl;
   outFile<<"0 30 80"<<endl<<endl;
-  outFile<<"#3 box数目 #每个box数据行数：4  ##之后可改为3"<<endl;
+  outFile<<"#3 box数目 "<<endl;
   outFile<<nofBox0*2<<endl<<endl;  ///to_string(nofBox0)
   outFile<<"##################################################################"<<endl;
   outFile<<"#- Boxid 这个Box下MRPC数目  ##"<<endl;
   outFile<<"#- Box:全长dx dy dz 位置x 位置y 位置z 殴拉角1(Z) 殴拉角2(Y) 殴拉角3(Z)  ##Box位置 作为下一级坐标的参考系   ##殴拉角单位：rad"<<endl;
-  outFile<<"##-- MRPC:Boxid-MRPCid dx dy dz 位置x 位置y 位置z 殴拉角1(Z) 殴拉角2(Y) 殴拉角3(Z) 层数（奇数）"<<endl;
+  outFile<<"##-- MRPC:Boxid-MRPCid dx dy dz 位置x 位置y 位置z 殴拉角1(Z) 殴拉角2(Y) 殴拉角3(Z) 层数"<<endl;
   outFile<<"###--- MRPC每层:Boxid-MRPCid-id dx dy dz 位置x 位置y 位置z ##约定dz为气隙/玻璃厚度"<<endl;
   outFile<<"##################################################################"<<endl<<endl;
 
 
   for(int i=1;i<=nofBox0;i++){
     outFile<<i<<" "<<"1"<<endl;
-    G4double phi_d=(i-1)*30;
+    //G4double phi_d=(i-1)*30;
     G4double phi_r=(i-1)*30*pi/180;
     G4double px = 18*sin(phi_r);
     G4double py = 18*cos(phi_r);
@@ -82,7 +83,7 @@ T1T0::T1T0()
   }
   for(int i=1;i<=nofBox0;i++){
     outFile<<i+nofBox0<<" "<<"1"<<endl;
-    G4double phi_d=15+(i-1)*30;
+    //G4double phi_d=15+(i-1)*30;
     G4double phi_r=(15+(i-1)*30)*pi/180;
     G4double px = 22.5*sin(phi_r);
     G4double py = 22.5*cos(phi_r);
@@ -114,230 +115,198 @@ T1T0::T1T0()
 
   fstream theFile;  //   ifstream theFile;
   theFile.open("./data/T0.dat",ios::in);
-  G4int hangshu=0;
-  string str;
   G4Tubs* solidT0;
   string nameT0="CEE_T0";
   G4NistManager* nist = G4NistManager::Instance();
-  G4int numofbox=0;
-  string nowboxid;
-  string nowMRPCid;
-
-  G4bool checkOverlaps = false;
-  hangshu=0;   // "将要读第hangshu+1行"
-  while(hangshu<=2){
-    if((getline(theFile,str))){
-      ////outFile <<"T1T0 构造函数 56  读入数据前三行"<<"  str:"<<str<<endl;
-      if(!str.empty()){
-        ////outFile <<"T1T0 构造函数 58  读入数据前三行"<<"  str:"<<str<<endl;
-        if(str[0]!=' '&&str[0]!='#'){
-          ////outFile <<"T1T0 构造函数 60  读入数据前三行"<<"  str:"<<str<<"  hangshu:"<<hangshu+1<<endl;
-          hangshu=hangshu+1;
-          if(hangshu==1){
-            vector<string> result=StringSplit(str," ");
-            G4ThreeVector T1T0_Position=G4ThreeVector(stod(result[0])*mm,stod(result[1])*mm,stod(result[2])*mm);
-            G4RotationMatrix boxrot;
-            /*
-            boxrot.rotateZ(box_eZ1*rad);
-            boxrot.rotateY(box_eY*rad);
-            boxrot.rotateZ(box_eZ2*rad);
-            */
-            transT0=G4Transform3D(boxrot, T1T0_Position);
-          }
-          else if(hangshu==2){
-            vector<string> result=StringSplit(str," ");
-            solidT0 = new G4Tubs(nameT0+"_solid",stod(result[0])*mm,stod(result[1])*mm,0.5*stod(result[2])*mm,0,2*pi);
-            G4Material* matT0 = nist->FindOrBuildMaterial("G4_AIR");                /////gai
-            G4VisAttributes* visT0 = new G4VisAttributes(G4Colour(0.1,0.4,1.5,0));  /////gai
-            logicT0 = new G4LogicalVolume(solidT0,matT0,nameT0+"_logic");
-            logicT0 -> SetVisAttributes(visT0);
-          }
-          else if(hangshu==3){
-            //outFile <<"T1T0 构造函数 79  读入数据前三行"<<"  str:"<<str<<endl;
-            vector<string> result=StringSplit(str," ");
-            //outFile <<"T1T0 构造函数 81  读入数据前三行"<<"  result:"<<result[0]<<endl;
-            numofbox=stoi(result[0]);
-          }
 
 
-        }
-      }
-    }
+
+  G4bool checkOverlaps = true;
+
+  ///hangshu=0;   // "将要读第hangshu+1行"
+
+  vector<string> result;
+  ///读第一行
+  ReadLine(theFile,result);
+  {
+    G4ThreeVector T1T0_Position=G4ThreeVector(stod(result[0])*mm,stod(result[1])*mm,stod(result[2])*mm);
+    G4RotationMatrix rot;
+    /*
+    rot.rotateZ(box_eZ1*rad);
+    rot.rotateY(box_eY*rad);
+    rot.rotateZ(box_eZ2*rad);
+    */
+    transT0=G4Transform3D(rot, T1T0_Position);
   }
-  //outFile <<"T1T0 构造函数 87  读入数据读完前三行"<<"  numofbox:"<<numofbox<<endl;
+
+
+  ///读第二行，构建TPC
+  ReadLine(theFile,result);
+  {
+    solidT0 = new G4Tubs(nameT0+"_solid",stod(result[0])*mm,stod(result[1])*mm,0.5*stod(result[2])*mm,0,2*pi);
+    G4Material* matT0 = nist->FindOrBuildMaterial("G4_AIR");                /////gai
+    G4VisAttributes* visT0 = new G4VisAttributes(G4Colour(0.1,0.4,1.5,0));  /////gai
+    logicT0 = new G4LogicalVolume(solidT0,matT0,nameT0+"_logic");
+    logicT0 -> SetVisAttributes(visT0);
+  }
+
+  ///读第三行
+  G4int numofbox;
+  ReadLine(theFile,result);
+  {
+    numofbox=stoi(result[0]);
+  }
+
+  ///循环读box
   for(int i=0;i<numofbox;i++){
-    G4int hangshu0=hangshu;
-    G4int numofMRPC=0;
+    ///读boxid
+    string nowboxid;
+    ReadLine(theFile,result);
+    {
+      nowboxid=result[0];
+    }
+
+
+    ///读box数据，构建box
     G4LogicalVolume* logicT0box;
-    while(hangshu==hangshu0){
-      if(getline(theFile,str)){
-        if(!str.empty()){
-          if(str[0]!=' '&&str[0]!='#'){
-            hangshu=hangshu+1;
-            //outFile <<"T1T0 构造函数 96  读入数据 4  str:"<<str<<"  i:"<<i<<endl;
-            vector<string> result=StringSplit(str," ");
-            nowboxid=result[0];
-            //numofMRPC=stoi(result[1]);
-          }
-        }
-      }
+    G4int numofMRPC;
+    ReadLine(theFile,result);
+    {
+      G4double box_dx,box_dy,box_dz;
+      G4double box_px,box_py,box_pz;
+      G4double box_eZ1,box_eY,box_eZ2;
+
+      box_dx=stod(result[0]);
+      box_dy=stod(result[1]);
+      box_dz=stod(result[2]);
+      box_px=stod(result[3]);
+      box_py=stod(result[4]);
+      box_pz=stod(result[5]);
+      box_eZ1=stod(result[6]);
+      box_eY=stod(result[7]);
+      box_eZ2=stod(result[8]);
+      numofMRPC=stoi(result[9]);
+
+
+      G4Box* solidT0box = new G4Box(nameT0+"_box_"+nowboxid+"_solid",0.5*box_dx*mm,0.5*box_dy*mm,0.5*box_dz*mm);
+      G4Material* matT0box = nist->FindOrBuildMaterial("G4_AIR");                /////gai
+      G4VisAttributes* visT0box = new G4VisAttributes(G4Colour(0.1,0.8,1.1,0));  /////gai
+      logicT0box = new G4LogicalVolume(solidT0box,matT0box,nameT0+"_box_"+nowboxid+"_logic");
+      logicT0box -> SetVisAttributes(visT0box);
+
+
+      G4ThreeVector boxp(box_px*mm,box_py*mm,box_pz*mm);
+      G4RotationMatrix boxrot;
+      boxrot.rotateZ(box_eZ1*rad);
+      boxrot.rotateY(box_eY*rad);
+      boxrot.rotateZ(box_eZ2*rad);
+      G4Transform3D boxtrans(boxrot, boxp);
+
+      new G4PVPlacement(boxtrans,
+                        logicT0box,            //its logical volume
+                        nameT0+"_box_"+nowboxid+"_phys",               //its name
+                        logicT0,                     //its mother  volume
+                        false,                 //no boolean operation
+                        0,                     //copy number
+                        checkOverlaps);        //overlaps checking
     }
-    //outFile <<"T1T0 构造函数 102  读入数据 4 numofMRPC:"<<numofMRPC<<endl;
-    hangshu0=hangshu;
-    while(hangshu==hangshu0){
-      if(getline(theFile,str)){
-        //outFile <<"T1T0 构造函数 111  读入数据 5"<<"  str:"<<str<<endl;
-        if(!str.empty()){
-          if(str[0]!=' '&&str[0]!='#'){
-            //outFile <<"T1T0 构造函数 114  读入数据 5"<<"  str:"<<str<<endl;
-            hangshu=hangshu+1;
 
-            G4double box_dx,box_dy,box_dz;
-            G4double box_px,box_py,box_pz;
-            G4double box_eZ1,box_eY,box_eZ2;
-            vector<string> result=StringSplit(str," ");
-            box_dx=stod(result[0]);
-            box_dy=stod(result[1]);
-            box_dz=stod(result[2]);
-            box_px=stod(result[3]);
-            box_py=stod(result[4]);
-            box_pz=stod(result[5]);
-            box_eZ1=stod(result[6]);
-            box_eY=stod(result[7]);
-            box_eZ2=stod(result[8]);
-            numofMRPC=stoi(result[9]);
-            //outFile <<"T1T0 构造函数 130  读入数据"<<"  box_eZ2:"<<box_eZ2<<endl;
-
-            G4Box* solidT0box = new G4Box(nameT0+"_box_"+nowboxid+"_solid",0.5*box_dx*mm,0.5*box_dy*mm,0.5*box_dz*mm);
-            G4Material* matT0box = nist->FindOrBuildMaterial("G4_AIR");                /////gai
-            G4VisAttributes* visT0box = new G4VisAttributes(G4Colour(0.1,0.8,1.1,0));  /////gai
-            logicT0box = new G4LogicalVolume(solidT0box,matT0box,nameT0+"_box_"+nowboxid+"_logic");
-            logicT0box -> SetVisAttributes(visT0box);
-            //outFile <<"T1T0 构造函数 137  读入数据 hangshu:"<<hangshu<<endl;
-
-            G4ThreeVector boxp(box_px*mm,box_py*mm,box_pz*mm);
-            G4RotationMatrix boxrot;
-            boxrot.rotateZ(box_eZ1*rad);
-            boxrot.rotateY(box_eY*rad);
-            boxrot.rotateZ(box_eZ2*rad);
-            G4Transform3D boxtrans(boxrot, boxp);
-
-            new G4PVPlacement(boxtrans,
-                              logicT0box,            //its logical volume
-                              nameT0+"_box_"+nowboxid+"_phys",               //its name
-                              logicT0,                     //its mother  volume
-                              false,                 //no boolean operation
-                              0,                     //copy number
-                              checkOverlaps);        //overlaps checking
-          }
-        }
-      }
-    }
-    //outFile <<"T1T0 构造函数 156  开始MRPC"<<"  numofMRPC:"<<numofMRPC<<endl;
+    ///循环读MRPC
     for(int j=0;j<numofMRPC;j++){
-      //outFile <<"T1T0 构造函数 158  开始MRPC"<<"  numofMRPC:"<<numofMRPC<<" j:"<<j<<endl;
-      G4int numofceng=0;
+      ///读MRPC数据，构建MRPC(MRPCbox)
       G4LogicalVolume* logicT0boxMRPC;
-      hangshu0=hangshu;
-      while(hangshu==hangshu0){
-        if(getline(theFile,str)){
-          if(!str.empty()){
-            if(str[0]!=' '&&str[0]!='#'){
-              hangshu=hangshu+1;
+      string nowMRPCid;
+      G4int numofceng;
+      ReadLine(theFile,result);
+      {
+        G4double box_dx,box_dy,box_dz;
+        G4double box_px,box_py,box_pz;
+        G4double box_eZ1,box_eY,box_eZ2;
 
-              G4double box_dx,box_dy,box_dz;
-              G4double box_px,box_py,box_pz;
-              G4double box_eZ1,box_eY,box_eZ2;
-              vector<string> result=StringSplit(str," ");
-              nowMRPCid = result[0];
-              box_dx=stod(result[1]);
-              box_dy=stod(result[2]);
-              box_dz=stod(result[3]);
-              box_px=stod(result[4]);
-              box_py=stod(result[5]);
-              box_pz=stod(result[6]);
-              box_eZ1=stod(result[7]);
-              box_eY=stod(result[8]);
-              box_eZ2=stod(result[9]);
-              numofceng=stoi(result[10]);
+        nowMRPCid = result[0];
+        box_dx=stod(result[1]);
+        box_dy=stod(result[2]);
+        box_dz=stod(result[3]);
+        box_px=stod(result[4]);
+        box_py=stod(result[5]);
+        box_pz=stod(result[6]);
+        box_eZ1=stod(result[7]);
+        box_eY=stod(result[8]);
+        box_eZ2=stod(result[9]);
+        numofceng=stoi(result[10]);
 
 
-              G4Box* solidT0boxMRPC = new G4Box(nameT0+"_box_"+nowboxid+"_MRPC_"+nowMRPCid+"_solid",0.5*box_dx*mm,0.5*box_dy*mm,0.5*box_dz*mm);
-              G4Material* matT0boxMRPC = nist->FindOrBuildMaterial("G4_AIR");                /////gai
-              G4VisAttributes* visT0boxMRPC = new G4VisAttributes(G4Colour(0.1,0.8,1.1,0));  /////gai
-              logicT0boxMRPC = new G4LogicalVolume(solidT0boxMRPC,matT0boxMRPC,nameT0+"_box_"+nowboxid+"_MRPC_"+nowMRPCid+"_logic");
-              logicT0boxMRPC -> SetVisAttributes(visT0boxMRPC);
+        G4Box* solidT0boxMRPC = new G4Box(nameT0+"_box_"+nowboxid+"_MRPC_"+nowMRPCid+"_solid",0.5*box_dx*mm,0.5*box_dy*mm,0.5*box_dz*mm);
+        G4Material* matT0boxMRPC = nist->FindOrBuildMaterial("G4_AIR");                /////gai
+        G4VisAttributes* visT0boxMRPC = new G4VisAttributes(G4Colour(0.1,0.8,1.1,0));  /////gai
+        logicT0boxMRPC = new G4LogicalVolume(solidT0boxMRPC,matT0boxMRPC,nameT0+"_box_"+nowboxid+"_MRPC_"+nowMRPCid+"_logic");
+        logicT0boxMRPC -> SetVisAttributes(visT0boxMRPC);
 
-              G4ThreeVector boxp(box_px*mm,box_py*mm,box_pz*mm);
-              G4RotationMatrix boxrot;
-              boxrot.rotateZ(box_eZ1*rad);
-              boxrot.rotateY(box_eY*rad);
-              boxrot.rotateZ(box_eZ2*rad);
-              G4Transform3D boxtrans(boxrot, boxp);
+        G4ThreeVector boxp(box_px*mm,box_py*mm,box_pz*mm);
+        G4RotationMatrix boxrot;
+        boxrot.rotateZ(box_eZ1*rad);
+        boxrot.rotateY(box_eY*rad);
+        boxrot.rotateZ(box_eZ2*rad);
+        G4Transform3D boxtrans(boxrot, boxp);
 
-              new G4PVPlacement(boxtrans,
-                                logicT0boxMRPC,            //its logical volume
-                                nameT0+"_box_"+nowboxid+"_MRPC_"+nowMRPCid+"_phys",               //its name
-                                logicT0box,                     //its mother  volume
-                                false,                 //no boolean operation
-                                0,                     //copy number
-                                checkOverlaps);        //overlaps checking
-
-            }
-          }
-        }
+        new G4PVPlacement(boxtrans,
+                          logicT0boxMRPC,            //its logical volume
+                          nameT0+"_box_"+nowboxid+"_MRPC_"+nowMRPCid+"_phys",               //its name
+                          logicT0box,                     //its mother  volume
+                          false,                 //no boolean operation
+                          0,                     //copy number
+                          checkOverlaps);        //overlaps checking
       }
+      ///循环读MRPCceng
       for(int k=0;k<numofceng;k++){
-        hangshu0=hangshu;
-        while(hangshu==hangshu0){
-          if(getline(theFile,str)){
-            if(!str.empty()){
-              if(str[0]!=' '&&str[0]!='#'){
-                hangshu=hangshu+1;
+        ///读MRPCceng数据，构建MRPCceng
+        string cengid;
+        ReadLine(theFile,result);
+        {
+          G4double box_dx,box_dy,box_dz;
+          G4double box_px,box_py,box_pz;
+          //G4double box_eZ1,box_eY,box_eZ2;
+          cengid = result[0];
+          box_dx=stod(result[1]);
+          box_dy=stod(result[2]);
+          box_dz=stod(result[3]);
+          box_px=stod(result[4]);
+          box_py=stod(result[5]);
+          box_pz=stod(result[6]);
+          //box_eZ1=stod(result[7]);
+          //box_eY=stod(result[8]);
+          //box_eZ2=stod(result[9]);
 
-                string cengid;
-                G4double box_dx,box_dy,box_dz;
-                G4double box_px,box_py,box_pz;
-                //G4double box_eZ1,box_eY,box_eZ2;
-                vector<string> result=StringSplit(str," ");
-                cengid = result[0];
-                box_dx=stod(result[1]);
-                box_dy=stod(result[2]);
-                box_dz=stod(result[3]);
-                box_px=stod(result[4]);
-                box_py=stod(result[5]);
-                box_pz=stod(result[6]);
-                //box_eZ1=stod(result[7]);
-                //box_eY=stod(result[8]);
-                //box_eZ2=stod(result[9]);
-
-                G4Box* solidT0boxMRPCceng = new G4Box(nameT0+"_box_"+nowboxid+"_MRPC_"+nowMRPCid+"_ceng_"+cengid+"_solid",0.5*box_dx*mm,0.5*box_dy*mm,0.5*box_dz*mm);
-                G4Material* matT0boxMRPCceng = nist->FindOrBuildMaterial("G4_AIR");                /////gai
-                G4VisAttributes* visT0boxMRPCceng = new G4VisAttributes(G4Colour(0.1,0.8,1.1,1));  /////gai
-                G4LogicalVolume* logicT0boxMRPCceng = new G4LogicalVolume(solidT0boxMRPCceng,matT0boxMRPCceng,nameT0+"_box_"+nowboxid+"_MRPC_"+nowMRPCid+"_ceng_"+cengid+"_logic");
-                logicT0boxMRPCceng -> SetVisAttributes(visT0boxMRPCceng);
-
-                G4ThreeVector boxp(box_px*mm,box_py*mm,box_pz*mm);
-                G4RotationMatrix boxrot;
-                /*
-                boxrot.rotateZ(box_eZ1*rad);
-                boxrot.rotateY(box_eY*rad);
-                boxrot.rotateZ(box_eZ2*rad);
-                */
-                G4Transform3D boxtrans(boxrot, boxp);
-
-                new G4PVPlacement(boxtrans,
-                                  logicT0boxMRPCceng,            //its logical volume
-                                  nameT0+"_box_"+nowboxid+"_MRPC_"+nowMRPCid+"_ceng_"+cengid+"_phys",               //its name
-                                  logicT0boxMRPC,                     //its mother  volume
-                                  false,                 //no boolean operation
-                                  0,                     //copy number
-                                  checkOverlaps);        //overlaps checking
-
-              }
-            }
+          G4Box* solidT0boxMRPCceng = new G4Box(nameT0+"_box_"+nowboxid+"_MRPC_"+nowMRPCid+"_ceng_"+cengid+"_solid",0.5*box_dx*mm,0.5*box_dy*mm,0.5*box_dz*mm);
+          /////gai
+          G4Material* matT0boxMRPCceng = nist->FindOrBuildMaterial("G4_AIR");                /////gai
+          G4VisAttributes* visT0boxMRPCceng = new G4VisAttributes(G4Colour(0.1,0.8,1.1,1));  /////gai
+          if(k%2!=0){
+            matT0boxMRPCceng = nist->FindOrBuildMaterial("G4_AIR");                /////gai
+            visT0boxMRPCceng = new G4VisAttributes(G4Colour(0.1,0.8,1.1,0.5));  /////gai
           }
+          /////gai
+          G4LogicalVolume* logicT0boxMRPCceng = new G4LogicalVolume(solidT0boxMRPCceng,matT0boxMRPCceng,nameT0+"_box_"+nowboxid+"_MRPC_"+nowMRPCid+"_ceng_"+cengid+"_logic");
+          logicT0boxMRPCceng -> SetVisAttributes(visT0boxMRPCceng);
+
+          G4ThreeVector boxp(box_px*mm,box_py*mm,box_pz*mm);
+          G4RotationMatrix boxrot;
+          /*
+          boxrot.rotateZ(box_eZ1*rad);
+          boxrot.rotateY(box_eY*rad);
+          boxrot.rotateZ(box_eZ2*rad);
+          */
+          G4Transform3D boxtrans(boxrot, boxp);
+
+          new G4PVPlacement(boxtrans,
+                            logicT0boxMRPCceng,            //its logical volume
+                            nameT0+"_box_"+nowboxid+"_MRPC_"+nowMRPCid+"_ceng_"+cengid+"_phys",               //its name
+                            logicT0boxMRPC,                     //its mother  volume
+                            false,                 //no boolean operation
+                            0,                     //copy number
+                            checkOverlaps);        //overlaps checking
         }
+
       }
     }
 
@@ -345,6 +314,5 @@ T1T0::T1T0()
 
   //outFile.close();
   theFile.close();
-
 
 }
---
+++ src/T1TPC.cc
@@ -0,0 +1,225 @@
+#include "T1TPC.hh"
+#include "G4VUserDetectorConstruction.hh"
+#include "G4LogicalVolume.hh"
+#include "G4PhysicalConstants.hh"
+#include "G4PVPlacement.hh"
+
+
+
+#include "G4NistManager.hh"
+#include "G4VisAttributes.hh"
+#include "G4Colour.hh"
+#include "G4Box.hh"
+#include "G4Tubs.hh"  //yuanzhu guanzi
+#include "G4Cons.hh"
+#include "G4Orb.hh"  //shixinqiu
+#include "G4Torus.hh"  //quan
+#include "G4Sphere.hh" //qiu  (duocanshu)
+#include "G4Trd.hh"
+
+#include "globals.hh"
+
+#include "G4SystemOfUnits.hh"
+
+//#include "T1MRPC.hh"
+#include <iostream>
+#include <string>
+#include <vector>
+#include "StringSplit.hh"
+#include "ReadLine.hh"
+
+using namespace std;
+/*
+void line_out_(ofstream& outFile, double var)
+{
+  outFile<<"1-1-1"<<" "<<"6"<<"0.55"<<" "<<"60"<<" "<<"0"<<" "<<"0"<<" "<<"0"<<" "<<"0"<<" "<<"0"<<" "<<"0"<<endl;
+}
+*/
+
+T1TPC::T1TPC()
+{
+
+  //ofstream //outFile;
+  ////outFile.open("error.txt",ios::app);
+  ////outFile <<"T1TPC 构造函数开始"<<endl;
+  G4int nofBox0 = 1;
+  ofstream outFile;
+  outFile.open("./data/TPC.dat",ios::app);
+  outFile<<"#1 相对于全局参考系的位置  殴拉角1(Z) 殴拉角2(Y) 殴拉角3(Z) ##这是下一级坐标的参考系  单位：mm   注释行开头为#，不支持行内注释"<<endl;
+  outFile<<"0 0 450 0 0 0"<<endl;
+  outFile<<"#2 dx dy dz（全长）"<<endl;
+  outFile<<"1050 850 850"<<endl<<endl;
+  outFile<<"#3 box数目 "<<endl;
+  outFile<<nofBox0<<endl<<endl;  ///to_string(nofBox0)
+  outFile<<"##################################################################"<<endl;
+  outFile<<"#- Boxid  ##"<<endl;
+  outFile<<"#- Box:全长dx dy dz 位置x 位置y 位置z 殴拉角1(Z) 殴拉角2(Y) 殴拉角3(Z)  ##Box位置 作为下一级坐标的参考系   ##殴拉角单位：rad"<<endl;
+  outFile<<"##-- TPC:Boxid-TPCid dx dy dz 位置x 位置y 位置z 殴拉角1(Z) 殴拉角2(Y) 殴拉角3(Z)"<<endl;
+  outFile<<"##################################################################"<<endl<<endl;
+
+
+  for(int i=1;i<=nofBox0;i++){
+
+    outFile<<i<<endl;
+    G4double px = 0;
+    G4double py = 0;
+    G4double pz = 0;
+
+    outFile<<"1050"<<" "<<"850"<<" "<<"850"<<" "<<px<<" "<<py<<" "<<pz<<" "<<"0"<<" "<<"0"<<" "<<"0"<<endl;//////
+    outFile<<"1-1"<<" "<<"1000"<<" "<<"800"<<" "<<"800"<<" "<<px<<" "<<py<<" "<<pz<<" "<<"0"<<" "<<"0"<<" "<<"0"<<endl;//////
+
+  }
+
+
+  outFile.close();
+
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+  fstream theFile;  //   ifstream theFile;
+  theFile.open("./data/TPC.dat",ios::in);
+  G4Box* solidTPC;
+  string nameTPC="CEE_TPC";
+  G4NistManager* nist = G4NistManager::Instance();
+
+
+
+  G4bool checkOverlaps = true;
+  ///hangshu=0;   // "将要读第hangshu+1行"
+
+  vector<string> result;
+  ///读第一行
+  ReadLine(theFile,result);
+  {
+    G4ThreeVector T1TPC_Position=G4ThreeVector(stod(result[0])*mm,stod(result[1])*mm,stod(result[2])*mm);
+    G4RotationMatrix rot;
+    /*
+    rot.rotateZ(stod(result[3])*rad);
+    rot.rotateY(stod(result[4])*rad);
+    rot.rotateZ(stod(result[5])*rad);
+    */
+    transTPC=G4Transform3D(rot, T1TPC_Position);
+  }
+
+  ///读第二行，构建TPC
+  ReadLine(theFile,result);
+  {
+    solidTPC = new G4Box(nameTPC+"_solid",stod(result[0])*mm,stod(result[1])*mm,0.5*stod(result[2])*mm);
+    G4Material* matTPC = nist->FindOrBuildMaterial("G4_AIR");                /////gai
+    G4VisAttributes* visTPC = new G4VisAttributes(G4Colour(0.1,0.4,1.5,0));  /////gai
+    logicTPC = new G4LogicalVolume(solidTPC,matTPC,nameTPC+"_logic");
+    logicTPC -> SetVisAttributes(visTPC);
+  }
+
+
+  ///读第三行
+  G4int numofbox;
+  ReadLine(theFile,result);
+  {
+    numofbox=stoi(result[0]);
+  }
+
+
+  ///循环读box
+  for(int i=0;i<numofbox;i++){
+    ///读boxid
+    string nowboxid;
+    ReadLine(theFile,result);
+    {
+      nowboxid=result[0];
+    }
+
+
+    ///读box数据，构建box
+    G4LogicalVolume* logicTPCbox;
+    ReadLine(theFile,result);
+    {
+      G4double box_dx,box_dy,box_dz;
+      G4double box_px,box_py,box_pz;
+      G4double box_eZ1,box_eY,box_eZ2;
+
+      box_dx=stod(result[0]);
+      box_dy=stod(result[1]);
+      box_dz=stod(result[2]);
+      box_px=stod(result[3]);
+      box_py=stod(result[4]);
+      box_pz=stod(result[5]);
+      box_eZ1=stod(result[6]);
+      box_eY=stod(result[7]);
+      box_eZ2=stod(result[8]);
+
+
+      G4Box* solidTPCbox = new G4Box(nameTPC+"_box_"+nowboxid+"_solid",0.5*box_dx*mm,0.5*box_dy*mm,0.5*box_dz*mm);
+      G4Material* matTPCbox = nist->FindOrBuildMaterial("G4_AIR");                /////gai
+      G4VisAttributes* visTPCbox = new G4VisAttributes(G4Colour(0.1,0.8,1.1,0));  /////gai
+      logicTPCbox = new G4LogicalVolume(solidTPCbox,matTPCbox,nameTPC+"_box_"+nowboxid+"_logic");
+      logicTPCbox -> SetVisAttributes(visTPCbox);
+
+
+      G4ThreeVector boxp(box_px*mm,box_py*mm,box_pz*mm);
+      G4RotationMatrix boxrot;
+      boxrot.rotateZ(box_eZ1*rad);
+      boxrot.rotateY(box_eY*rad);
+      boxrot.rotateZ(box_eZ2*rad);
+      G4Transform3D boxtrans(boxrot, boxp);
+
+      new G4PVPlacement(boxtrans,
+                        logicTPCbox,            //its logical volume
+                        nameTPC+"_box_"+nowboxid+"_phys",               //its name
+                        logicTPC,                     //its mother  volume
+                        false,                 //no boolean operation
+                        0,                     //copy number
+                        checkOverlaps);        //overlaps
+    }
+
+    ///读TPCchamber
+    G4LogicalVolume* logicTPCboxchamber;
+    string nowchamberid;
+    ReadLine(theFile,result);
+    {
+      G4double box_dx,box_dy,box_dz;
+      G4double box_px,box_py,box_pz;
+      G4double box_eZ1,box_eY,box_eZ2;
+      nowchamberid = result[0];
+      box_dx=stod(result[1]);
+      box_dy=stod(result[2]);
+      box_dz=stod(result[3]);
+      box_px=stod(result[4]);
+      box_py=stod(result[5]);
+      box_pz=stod(result[6]);
+      box_eZ1=stod(result[7]);
+      box_eY=stod(result[8]);
+      box_eZ2=stod(result[9]);
+
+
+      G4Box* solidTPCboxchamber = new G4Box(nameTPC+"_box_"+nowboxid+"_chamber_"+nowchamberid+"_solid",0.5*box_dx*mm,0.5*box_dy*mm,0.5*box_dz*mm);
+      G4Material* matTPCboxchamber= nist->FindOrBuildMaterial("G4_AIR");                /////gai
+      G4VisAttributes* visTPCboxchamber = new G4VisAttributes(G4Colour(0.1,0.8,0.2,0.5));  /////gai
+      logicTPCboxchamber = new G4LogicalVolume(solidTPCboxchamber,matTPCboxchamber,nameTPC+"_box_"+nowboxid+"_chamber_"+nowchamberid+"_logic");
+      logicTPCboxchamber -> SetVisAttributes(visTPCboxchamber);
+
+      G4ThreeVector boxp(box_px*mm,box_py*mm,box_pz*mm);
+      G4RotationMatrix boxrot;
+      boxrot.rotateZ(box_eZ1*rad);
+      boxrot.rotateY(box_eY*rad);
+      boxrot.rotateZ(box_eZ2*rad);
+      G4Transform3D boxtrans(boxrot, boxp);
+
+      new G4PVPlacement(boxtrans,
+                        logicTPCboxchamber,            //its logical volume
+                        nameTPC+"_box_"+nowboxid+"_chamber_"+nowchamberid+"_phys",               //its name
+                        logicTPCbox,                     //its mother  volume
+                        false,                 //no boolean operation
+                        0,                     //copy number
+                        checkOverlaps);        //overlaps checking
+    }
+
+  }
+  //outFile.close();
+  theFile.close();
+}
