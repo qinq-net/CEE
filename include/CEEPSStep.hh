d2ef161355199dd0e3f3d612f8d49e8ea9cf5edc
QIN Yuhao <qinq_net@buaa.edu.cn>
2018-05-11 18:31:19 +0800

    CEEPSStep: 用于记录每步主要信息的记录器

---
+++ include/CEEPSStep.hh
@@ -0,0 +1,44 @@
+#ifndef CEEPSStep_H
+#define CEEPSStep_H
+
+#include <G4VPrimitiveScorer.hh>
+#include <G4THitsMap.hh>
+#include <G4ThreeVector.hh>
+
+/* A struct used to save all valuable information during a major event */
+typedef struct
+{
+	G4int trackID;
+	G4StepPoint* preStepPoint;
+	G4StepPoint* postStepPoint;
+	G4double deltaEnergy;
+	G4double energyDeposit;
+} CEEStepData;
+
+class CEEPSStep : public G4VPrimitiveScorer
+{
+	public:
+	CEEPSStep(G4String name, G4int depth=0);
+	CEEPSStep(G4String name, G4bool onlymain=TRUE, G4int depth=0);
+	virtual ~CEEPSStep();
+
+	protected:
+	virtual G4bool ProcessHits(G4Step*, G4TouchableHistory*);
+
+	public:
+	virtual void Initialize(G4HCofThisEvent*);
+	virtual void EndOfEvent(G4HCofThisEvent*);
+	virtual void clear();
+	virtual void DrawAll();
+	virtual void PrintAll();
+
+	//virtual void SetUnit(const G4String& unit);
+
+	private:
+	G4int HCID;
+	G4THitsMap< std::vector<CEEStepData*> >* EvtMap;
+	G4bool onlymain;
+
+};
+
+#endif//CEEPSStep_H
---
+++ src/CEEPSStep.cc
@@ -0,0 +1,92 @@
+#include "CEEPSStep.hh"
+#include <G4UnitsTable.hh>
+#include <G4Step.hh>
+#include <G4SystemOfUnits.hh>
+
+CEEPSStep::CEEPSStep(G4String name, G4int depth)
+	: G4VPrimitiveScorer(name, depth), HCID(-1), EvtMap(0), onlymain(TRUE)
+{
+	;
+}
+
+CEEPSStep::CEEPSStep(G4String name, G4bool onlymain, G4int depth)
+	: G4VPrimitiveScorer(name, depth), HCID(-1), EvtMap(0)
+{
+	this->onlymain = onlymain;
+}
+
+CEEPSStep::~CEEPSStep()
+{
+	;
+}
+
+G4bool CEEPSStep::ProcessHits(G4Step* aStep, G4TouchableHistory*)
+{
+	G4int trackID = aStep->GetTrack()->GetTrackID();
+	if(!(onlymain) || aStep->GetTrack()->GetTrackID() == 0)
+	{
+		G4StepPoint* preStepPoint = aStep->GetPreStepPoint();
+		G4StepPoint* postStepPoint = aStep->GetPostStepPoint();
+		G4double deltaEnergy = aStep->GetDeltaEnergy();
+		G4double energyDeposit = aStep->GetTotalEnergyDeposit();
+		CEEStepData* stepData = new CEEStepData({
+			trackID,
+			preStepPoint,
+			postStepPoint,
+			deltaEnergy,
+			energyDeposit
+		});
+		
+		G4int index = GetIndex(aStep);
+		if(!(*EvtMap)[index]) EvtMap->set(index, *(new std::vector<CEEStepData*>));
+		(*EvtMap)[index]->push_back(stepData);
+		return TRUE;
+	}
+	return FALSE;
+}
+
+void CEEPSStep::Initialize(G4HCofThisEvent* HCE)
+{
+	EvtMap = new G4THitsMap< std::vector<CEEStepData*> >(GetMultiFunctionalDetector()->GetName(), GetName());
+	if(HCID < 0) {HCID = GetCollectionID(0);}
+	HCE->AddHitsCollection(HCID, (G4VHitsCollection*)EvtMap);
+}
+
+void CEEPSStep::EndOfEvent(G4HCofThisEvent*)
+{
+	;
+}
+
+void CEEPSStep::clear()
+{
+	EvtMap->clear();
+}
+
+void CEEPSStep::DrawAll()
+{
+	/* TODO: draw all points */
+	;
+}
+
+void CEEPSStep::PrintAll()
+{
+	G4cout << ">> MultiFunctionalDet  " << detector->GetName() << G4endl;
+	G4cout << ">> PrimitiveScorer " << GetName() << G4endl;
+	G4cout << ">> Number of entries " << EvtMap->entries() << G4endl;
+	for(auto copyItr: *(EvtMap->GetMap()))
+	{
+		G4cout 	<< " >   copy no.: " << copyItr.first << G4endl;
+		for(auto trackItr: *copyItr.second)
+		{
+			G4cout 	<< " >> t=" << trackItr->postStepPoint->GetGlobalTime()/ns
+				<< "ns x=" << trackItr->postStepPoint->GetPosition().x()/mm
+				<< "mm y=" << trackItr->postStepPoint->GetPosition().y()/mm
+				<< "mm z=" << trackItr->postStepPoint->GetPosition().z()/mm
+				<< "mm dE=" << trackItr->deltaEnergy/MeV
+				<< "MeV eD=" << trackItr->energyDeposit/MeV << "MeV" << G4endl;
+		}
+	}
+}
+		
+		
+	
